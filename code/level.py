030476e02ffaa02bd7272cf887aafb23bc25b86d
Nick Boodoo <boodoo@temple.edu>
2023-10-03 23:59:49 -0400

    created camera, fixed collisions, added hitboxes

--- code/level.py
+++ code/level.py
@@ -1,4 +1,6 @@
+from typing import Iterable, Union
 import pygame
+from pygame.sprite import AbstractGroup
 from settings import *
 from tile import Tile
 from player import Player
@@ -11,7 +13,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite groups setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
         # sprite setup
@@ -23,15 +25,38 @@ class Level:
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
+
+                # obstacle object
                 if col == 'x':
                     Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
+
+                # player object
                 if col == 'p':
                     self.player = Player((x,y), [self.visible_sprites], self.obstacles_sprites)
 
     def run(self):
 
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
+    
+
 
