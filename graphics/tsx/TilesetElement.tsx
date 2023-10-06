5120eb71d79ad743b2771bba373a039c3b00a4df
Nick Boodoo <boodoo@temple.edu>
2023-10-06 01:55:04 -0400

    added tilesets

---
+++ graphics/tsx/TilesetElement.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TilesetElement" tilewidth="64" tileheight="64" tilecount="240" columns="16">
+ <image source="../../../TilesetElement.png" width="1024" height="960"/>
+</tileset>
---
+++ graphics/tsx/TilesetHole.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TilesetHole" tilewidth="64" tileheight="64" tilecount="55" columns="11">
+ <image source="../../../TilesetHole.png" width="704" height="320"/>
+</tileset>
